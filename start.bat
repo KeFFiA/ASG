f75e3ff7809d581e8ad1b404293d7d3b0944739e
KeFFiA <ogienko.12003@gmail.com>
2025-03-13 00:29:34 +0300

    Add | start.bat

---
+++ start.bat
@@ -0,0 +1,5 @@
+%1@mshta vbscript:Execute("CreateObject(""Wscript.Shell"").Run """"""%~f0"""" :"",0:Close()")& exit/b
+
+@echo off
+
+C:\ASGBuilds\ICAO\venv\Scripts\python.exe C:\ASGBuilds\ICAO\main.py
